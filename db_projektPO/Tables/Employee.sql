c137c50a958f407965bbabb2d6d114acf5f61698
PawLie <pawsimek@gmail.com>
2021-06-18 16:53:11 +0200

    Database

---
+++ db_projektPO/Tables/Employee.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Employee]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [FirstName] NCHAR(100) NULL, 
+    [LastName] NCHAR(100) NULL, 
+    [PartyCode] NCHAR(10) NULL, 
+    [Email] NCHAR(250) NULL, 
+    [Phone] NCHAR(9) NULL
+)
